8d216d50601a944545db24ac7ed2c9e8f6aec359
soft as HELL <softashell@cock.li>
2018-02-15 02:37:36 +0200

    Clean up version formatting

--- SSELauncher/Forms/FrmAbout.cs
+++ SSELauncher/Forms/FrmAbout.cs
@@ -14,37 +14,26 @@ namespace SSELauncher
 
         private void FrmAbout_Load(object sender, EventArgs e)
 		{
-			try
-			{
-				FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "SmartSteamEmu\\SmartSteamEmu.dll"));
-                lblVersionx86.Text = "SmartSteamEmu.dll version " + string.Format("{0}.{1}.{2}.{3}", new object[]
-				{
-					versionInfo.FileMajorPart,
-					versionInfo.FileMinorPart,
-					versionInfo.FileBuildPart,
-					versionInfo.FilePrivatePart
-				});
-			}
-			catch
-			{
+            try
+            {
+                var versionInfo = FileVersionInfo.GetVersionInfo(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "SmartSteamEmu\\SmartSteamEmu.dll"));
+                lblVersionx86.Text = String.Format("SmartSteamEmu.dll version {0}.{1}.{2}.{3}", versionInfo.FileMajorPart, versionInfo.FileMinorPart, versionInfo.FileBuildPart, versionInfo.FilePrivatePart);
+            }
+            catch
+            {
                 lblVersionx86.Text = "Unable to retrieve SmartSteamEmu.dll version";
-			}
-			try
-			{
-				FileVersionInfo versionInfo2 = FileVersionInfo.GetVersionInfo(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "SmartSteamEmu\\SmartSteamEmu64.dll"));
-                lblVersionx64.Text = "SmartSteamEmu64.dll version " + string.Format("{0}.{1}.{2}.{3}", new object[]
-				{
-					versionInfo2.FileMajorPart,
-					versionInfo2.FileMinorPart,
-					versionInfo2.FileBuildPart,
-					versionInfo2.FilePrivatePart
-				});
-			}
-			catch
-			{
+            }
+
+            try
+            {
+                var versionInfo = FileVersionInfo.GetVersionInfo(Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "SmartSteamEmu\\SmartSteamEmu64.dll"));
+                lblVersionx64.Text = String.Format("SmartSteamEmu64.dll version {0}.{1}.{2}.{3}", versionInfo.FileMajorPart, versionInfo.FileMinorPart, versionInfo.FileBuildPart, versionInfo.FilePrivatePart);
+            }
+            catch
+            {
                 lblVersionx64.Text = "Unable to retrieve SmartSteamEmu64.dll version";
-			}
-		}
+            }
+        }
 
 		private void btnClose_Click(object sender, EventArgs e)
 		{
